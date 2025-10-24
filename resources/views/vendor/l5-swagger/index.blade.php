e96643b53f569141d28d87afa2a714504a7f815e
gorguimarena <gorguimarena@gmail.com>
2025-10-24 03:04:26 +0000

    gestion des erreurs swagger

--- resources/views/vendor/l5-swagger/index.blade.php
+++ resources/views/vendor/l5-swagger/index.blade.php
@@ -1,3 +1,9 @@
+@php
+    $documentation = $documentation ?? config('l5-swagger.default');
+    $documentationTitle = $documentationTitle ?? config('app.name', 'API Documentation');
+    $useAbsolutePath = $useAbsolutePath ?? false;
+@endphp
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,4 @@ Route::middleware('api')->group(function () {
     Route::prefix('comptes')->group(function () {
         Route::get('/', [CompteController::class, 'index']);
     });
-
-    Route::get('/documentation', function () {
-        return view('vendor.l5-swagger.index');
-    })->name('l5-swagger.default.api');
 });
