8495fe12c5fa7b3b417d6d4b23d2d9bda5dec753
gorguimarena <gorguimarena@gmail.com>
2025-10-24 13:33:44 +0000

    page blanche gestion-2 dans le file swagger

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/docs/asset/{file}', function ($file) {
+    $path = base_path('vendor/swagger-api/swagger-ui/dist/' . $file);
+    if (file_exists($path)) {
+        return response()->file($path);
+    }
+    abort(404);
+})->where('file', '.*');
